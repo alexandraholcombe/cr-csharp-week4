7daa32097b7fc2b05b85607b7358409558783972
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-03-03 10:47:27 -0800

    Duplicate all Band class methods and tests for Venue class

---
+++ Objects/Venue.cs
@@ -0,0 +1,152 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BandTracker.Objects
+{
+    public class Venue
+    {
+        private int _id;
+        private string _name;
+
+        public Venue(string name, int id = 0)
+        {
+            _name = name;
+            _id = id;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public int GetId()
+        {
+            return _id;
+        }
+
+        //Does some stuff involving equals who knows what
+        public override bool Equals(System.Object otherVenue)
+        {
+            if(!(otherVenue is Venue))
+            {
+                return false;
+            }
+            else
+            {
+                Venue newVenue = (Venue) otherVenue;
+                bool idEquality = (this.GetId() == newVenue.GetId());
+                bool nameEquality = (this.GetName() == newVenue.GetName());
+
+                return (idEquality && nameEquality);
+            }
+        }
+
+        //Delete all rows in the venues table
+        public static void DeleteAll()
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("DELETE FROM venues;", conn);
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+        }
+
+        //Output all rows in venues table to List
+        public static List<Venue> GetAll()
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM venues ORDER BY name;", conn);
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            List<Venue> allVenues = new List<Venue>{};
+
+            while(rdr.Read())
+            {
+                int id = rdr.GetInt32(0);
+                string name = rdr.GetString(1);
+
+                Venue newVenue = new Venue(name, id);
+                allVenues.Add(newVenue);
+            }
+
+            if(rdr != null)
+            {
+                rdr.Close();
+            }
+
+            if(conn != null)
+            {
+                conn.Close();
+            }
+
+            return allVenues;
+        }
+
+        //Saves instances to databasejj
+        public void Save()
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO venues (name) OUTPUT INSERTED.id VALUES (@VenueName);", conn);
+            cmd.Parameters.Add(new SqlParameter("@VenueName", this.GetName()));
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while(rdr.Read())
+            {
+                this._id = rdr.GetInt32(0);
+            }
+
+            if(rdr != null)
+            {
+                rdr.Close();
+            }
+
+            if(conn != null)
+            {
+                conn.Close();
+            }
+        }
+
+        //Finds instance in database
+        public static Venue Find(int id)
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM venues WHERE id = @VenueId;", conn);
+            cmd.Parameters.Add(new SqlParameter("@VenueId", id.ToString()));
+
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            int foundId = 0;
+            string foundName = null;
+
+            while(rdr.Read())
+            {
+                foundId = rdr.GetInt32(0);
+                foundName = rdr.GetString(1);
+            }
+            Venue foundVenue = new Venue(foundName, foundId);
+
+            if(rdr != null)
+            {
+                rdr.Close();
+            }
+
+            if(conn != null)
+            {
+                conn.Close();
+            }
+
+            return foundVenue;
+        }
+
+    }
+}
---
+++ Tests/VenueTest.cs
@@ -0,0 +1,115 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BandTracker.Objects
+{
+    public class VenueTest: IDisposable
+    {
+        public VenueTest()
+        {
+            DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=band_tracker_test;Integrated Security=SSPI;";
+        }
+
+        //Delete everything between tests
+        public void Dispose()
+        {
+            Venue.DeleteAll();
+        }
+
+        //GetAll returns empty list if no venues
+        [Fact]
+        public void GetAll_ForNoVenues_EmptyList()
+        {
+            //Arrange, Act, Assert
+            List<Venue> actualResult = Venue.GetAll();
+            List<Venue> expectedResult = new List<Venue>{};
+
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        //Checks for venues table has zero rows at beginning of test
+        [Fact]
+        public void Test_ForNoRowsInVenuesTable()
+        {
+            int actualResult = Venue.GetAll().Count;
+            int expectedResult = 0;
+
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        //Checks if EqualOverride is working whatever that does because it clearly does not do what I thought it was doing before
+        [Fact]
+        public void EqualOverride_VenueNamesAreSame_true()
+        {
+            //Arrange, Act
+            Venue firstVenue = new Venue("The Station");
+            Venue secondVenue = new Venue("The Station");
+
+            //Assert
+            Assert.Equal(firstVenue, secondVenue);
+        }
+
+        //Checks if instances are saved to database
+        [Fact]
+        public void Save_ForVenue_SavesToDatabase()
+        {
+            //Arrange
+            Venue newVenue = new Venue("The Station");
+
+            //Act
+            newVenue.Save();
+
+            //Assert
+            List<Venue> actualResult = Venue.GetAll();
+            List<Venue> expectedResult = new List<Venue>{newVenue};
+        }
+
+        //Checks that database id is assigned when saved to db
+        [Fact]
+        public void Save_ForVenue_AssignsDatabaseId()
+        {
+            //Arrange
+            Venue testVenue = new Venue("The Station");
+
+            //Act
+            testVenue.Save();
+            Venue savedVenue = Venue.GetAll()[0];
+
+            //Assert
+            Assert.Equal(testVenue, savedVenue);
+        }
+
+        //Checks that GetAll method works for multiple instances
+        [Fact]
+        public void GetAll_ForMultipleVenues_ReturnsListWithAllVenues()
+        {
+            //Arrange
+            Venue firstVenue = new Venue("The Station");
+            Venue secondVenue = new Venue("Club Fiber");
+            firstVenue.Save();
+            secondVenue.Save();
+
+            //Act, Assert
+            List<Venue> actualResult = Venue.GetAll();
+            List<Venue> expectedResult = new List<Venue> {secondVenue, firstVenue};
+
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        //Checks that Find method finds correct venue in databasejj
+        [Fact]
+        public void Find_ForVenue_FindsVenueInDatabase()
+        {
+            //Arrange
+            Venue testVenue = new Venue("The Station");
+            testVenue.Save();
+
+            //Act, Assert
+            Venue foundVenue = Venue.Find(testVenue.GetId());
+            Assert.Equal(testVenue, foundVenue);
+        }
+    }
+}

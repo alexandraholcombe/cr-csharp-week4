fe1f8a5f5cd4e05cc5346b43761417a8666f165d
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-03-03 10:43:51 -0800

    Add Find method for Band class

--- Objects/Band.cs
+++ Objects/Band.cs
@@ -114,5 +114,39 @@ namespace BandTracker.Objects
             }
         }
 
+        //Finds instance in database
+        public static Band Find(int id)
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM bands WHERE id = @BandId;", conn);
+            cmd.Parameters.Add(new SqlParameter("@BandId", id.ToString()));
+
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            int foundId = 0;
+            string foundName = null;
+
+            while(rdr.Read())
+            {
+                foundId = rdr.GetInt32(0);
+                foundName = rdr.GetString(1);
+            }
+            Band foundBand = new Band(foundName, foundId);
+
+            if(rdr != null)
+            {
+                rdr.Close();
+            }
+
+            if(conn != null)
+            {
+                conn.Close();
+            }
+
+            return foundBand;
+        }
+
     }
 }

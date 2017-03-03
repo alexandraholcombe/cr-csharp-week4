1581817becad45d871cc437ba2b6b101b7ce1587
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-03-03 14:16:26 -0800

    Add forms to add new bands

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -22,6 +22,28 @@ namespace BandTracker
                 List<Venue> allVenues = Venue.GetAll();
                 return View["venues.cshtml", allVenues];
             };
+
+            Get["/bands/new"] = _ => {
+                return View["newband.cshtml"];
+            };
+
+            Post["/bands"] = _ => {
+                Band newBand = new Band(Request.Form["band-name"]);
+                newBand.Save();
+                List<Band> allBands = Band.GetAll();
+                return View["bands.cshtml", allBands];
+            };
+
+            Get["/venues/new"] = _ => {
+                return View["newvenue.cshtml"];
+            };
+
+            Post["/venues"] = _ => {
+                Venue newVenue = new Venue(Request.Form["venue-name"]);
+                newVenue.Save();
+                List<Venue> allVenues = Venue.GetAll();
+                return View["venues.cshtml", allVenues];
+            };
         }
     }
 }
